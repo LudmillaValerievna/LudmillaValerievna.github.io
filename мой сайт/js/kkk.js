151e690b2bb58078f9f7b540269bb94b6bbcad9b
LudmillaValerievna <LudmillaValerievna@english.org>
2020-02-20 22:00:01 +0300

    Commit Site

---
+++ мой сайт/js/kkk.js
@@ -0,0 +1,20 @@
+<script>
+    $(document).ready(function () {
+        $('form').submit(function () {
+            var formID = $(this).attr('id'); // Получение ID формы
+            var formNm = $('#' + formID);
+            $.ajax({
+                type: 'POST',
+                url: 'feedback.php', // Обработчик формы отправки
+                data: formNm.serialize(),
+                success: function (data) {
+                    // Вывод текста результата отправки в текущей форме
+                    $(formNm).html(data);
+                }
+            });
+            return false;
+        });
+    });
+</script>
+
+
